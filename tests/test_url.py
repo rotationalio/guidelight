7d97e7f0b0ecf1390748569c3dffc94f2c3a81ab
Benjamin Bengfort <benjamin@rotational.io>
2025-09-24 10:28:45 -0500

    URL Handling (#1)
    Implements URL handling for endpoint creation.

---
+++ tests/test_url.py
@@ -0,0 +1,77 @@
+import pytest
+
+from guidelight.url import URL, parse_content_type
+
+
+@pytest.mark.parametrize(
+    "url",
+    [
+        "http://example.com",
+        "https://example.com/path/to/resource?query=param#fragment",
+        "ftp://ftp.example.com/resource",
+        "http://example.com:8080",
+        "http://localhost",
+        "http://localhost:9000/path",
+        "http://example.local",
+    ],
+)
+def test_url_parse(url):
+    parsed = URL.parse(url)
+    assert str(parsed) == url
+
+
+@pytest.mark.parametrize(
+    "url,endpoint,query,expected",
+    [
+        (
+            "https://guidelight.dev",
+            ("v1", "agents"),
+            None,
+            "https://guidelight.dev/v1/agents",
+        ),
+        (
+            "https://guidelight.dev/v1",
+            ("agents",),
+            None,
+            "https://guidelight.dev/v1/agents",
+        ),
+        (
+            "https://guidelight.dev/v1",
+            ("/", "v2", "agents"),
+            None,
+            "https://guidelight.dev/v2/agents",
+        ),
+        (
+            "https://guidelight.dev?ordering=created",
+            ("v1", "agents"),
+            None,
+            "https://guidelight.dev/v1/agents?ordering=created",
+        ),
+        (
+            "https://guidelight.dev?ordering=created",
+            ("v1", "agents"),
+            {"ordering": "modified"},
+            "https://guidelight.dev/v1/agents?ordering=modified",
+        ),
+        (
+            "https://guidelight.dev?ordering=created",
+            ("v1", "agents"),
+            {"include": "archives"},
+            "https://guidelight.dev/v1/agents?ordering=created&include=archives",
+        ),
+    ],
+)
+def test_resolve(url, endpoint, query, expected):
+    base = URL.parse(url)
+    resolved = base.resolve(*endpoint, query=query)
+    assert str(resolved) == expected
+
+
+@pytest.mark.parametrize("input,mime,params", [
+    ("text/html; charset=UTF-8", "text/html", {"charset": "UTF-8"}),
+    ("application/json", "application/json", {}),
+])
+def test_parse_content_type(input, mime, params):
+    result_mime, result_params = parse_content_type(input)
+    assert result_mime == mime
+    assert result_params == params
